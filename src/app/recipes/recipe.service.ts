e8b3fbc212c82c7710ddb8c043eefe0c74eac93d
Yash Mishra <yashmishra12@hotmail.com>
2020-08-11 23:05:39 +0530

    Ingredients added to Recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService{
 
@@ -8,9 +9,24 @@ export class RecipeService{
 
   private recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
-    new Recipe('Pizza', 'Five Pepper Pizza', 'https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+    new Recipe('Pizza',
+      'Five Pepper Pizza',
+      'https://images.pexels.com/photos/803290/pexels-photo-803290.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      [
+        new Ingredient('Jalepano', 1),
+        new Ingredient('Corn', 20),
+        new Ingredient ('Mushroom', 5),
+        new Ingredient('Cheese', 3)
+      ]),
     // tslint:disable-next-line:max-line-length
-    new Recipe('Cherry', 'Delicious Cherry Dessert', 'https://images.pexels.com/photos/35629/bing-cherries-ripe-red-fruit.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
+    new Recipe('Burger',
+      'Delicious Veg Burger',
+      'https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      [
+        new Ingredient('Buns', 1),
+        new Ingredient('Lettuce', 5),
+        new Ingredient('Tomato', 6)
+      ])
   ];
 
   getRecipes() {
