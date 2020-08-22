e6462b8d02113a1269bc48d6ea15ee95d2101daa
Yash Mishra <yashmishra12@hotmail.com>
2020-08-23 01:54:30 +0530

    Manage Recipes dropdown fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import {SharedModule} from './shared/shared.module';
 
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
